a7a79a8d661412104f8ff0e4e3d672f0054b6060
shubhangij12 <jainshubhangi97@gmail.com>
2020-05-30 17:07:41 +0530

    site

---
+++ register/views.py
@@ -0,0 +1,16 @@
+# views.py
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+# Create your views here.
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("http://127.0.0.1:8000/login")
+    else:
+        form = RegisterForm()
+
+    return render(response, "register/register.html", {"form":form})
